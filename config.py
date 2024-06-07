6dcc273bd700cd15138a978cb1cf62e68a7889b4
R0gal1k <95590314+R0gfall@users.noreply.github.com>
2024-06-07 21:09:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,1 @@
 
-# TOKEN_API = '7156628879:AAF19RNicALT57A_Q5_hna-DhNe6mTjPNJk'
-TOKEN_API = '5105838748:AAGyTYZD3G1B9aFw3Yy-mvIOVK9dco6I2SI'
-
-host = 'localhost'
-port = 5432
-user = 'postgres'
-password = 'DecT725f666des'
-database_name = 'bot_ai_3'
-
-image_path = 'C:/Users/PCLe/Lab3/TGbot/TGbotAI/content/image_test/new_image.jpg'
-model5_path = "machine_learn/model.h5"
-
-download_image_path = 'C:/Users/PCLe/Lab3/TGbot/TGbotAI/content/image_test/new_image.jpg'
